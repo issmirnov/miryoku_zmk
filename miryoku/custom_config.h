65fb5367c4d1b396af90fc81ea2fef4254348117
Ivan Smirnov <isgsmirnov@gmail.com>
2022-07-28 22:19:05 -0600

    remove commented out layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -116,11 +116,6 @@ U_NP,              U_NP,              &lt U_NUM ESC,     &lt U_NAV SPC,     &lt 
 
 
 // Layer 1: Button, use default
-/* #define MIRYOKU_LAYER_BUTTON \ */
-/* U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \ */
-/* &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         &to U_GAME,             &none,             &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \ */
-/* U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \ */
-/* U_NP,              U_NP,              U_BTN2,            U_BTN3,            U_BTN1,            U_BTN1,            U_BTN3,            U_BTN2,            U_NP,              U_NP */
 
 // Layer 2: Nav, use default.
 
